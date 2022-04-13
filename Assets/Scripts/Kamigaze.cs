dc968932b4c52c75ce449fadba3240e5f424304d
q1w2e3hhhh5 <salahedineraquib@gmail.com>
2022-04-13 15:12:12 -0400

    managed to make kamikage disapear alongside with asteriod upon collision

--- Assets/Scripts/Kamigaze.cs
+++ Assets/Scripts/Kamigaze.cs
@@ -7,22 +7,48 @@ public class Kamigaze : MonoBehaviour
 
     private GameObject playerObj = null;
     public float movementSpead = 0f, rotationSpeed = 150f;
-    public GameObject explosiont;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerObj = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.position += playerObj.transform.position * Time.deltaTime;
+       // transform.position = Vector3.Rotate(0, 0, movementSpead * Time.deltaTime, 0.0f);
+
+
         transform.position = Vector3.MoveTowards(transform.position, playerObj.transform.position, movementSpead * Time.deltaTime);
+
         Debug.Log("Player position is: " + playerObj.transform.position);
         Debug.Log("Kamikaze position is: " + transform.position);
     }
+
+    public void Explode()
+    {
+        Destroy(gameObject);//kill the asteroid
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Asteroid"))
+        {
+            Destroy(gameObject); //kms
+
+            //how do i make it boom
+
+            other.transform.GetComponent<Asteroid>()?.Explode(); 
+
+        }
+    }
+
+
+
 }
